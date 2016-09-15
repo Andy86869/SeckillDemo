3a84cc5b5aa85d9713b7c7e60be586ab530dd18b
Andy <robbincen@163.com>
2016-09-15 17:13:58 +0800

    SeckillService 单元测试文件

---
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,66 @@
+package org.seckill.service;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
+public class SeckillServiceTest {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private SeckillService seckillService;
+
+	@Test
+	public void testGetSeckillList() {
+		List<Seckill> list = seckillService.getSeckillList();
+		logger.info("list={}",list);
+	}
+
+	@Test
+	public void testGetById() {
+		long id = 1005;
+		Seckill seckill = seckillService.getById(id);
+		logger.info("seckill={}",seckill);
+	}
+	
+	//测试代码完整逻辑,注意可以重复执行.
+	@Test
+	public void testSeckillLogic() {
+		
+		/**
+		 * exposer=Exposer [exposed=false, md5=null, seckillId=1005, now=1473926392034, start=1473955200000, end=1474041600000]
+		 * exposer=Exposer [exposed=true, md5=06586154e01562b5e7f482d15ed7924d, seckillId=1005, now=0, start=0, end=0]
+		 */
+		long id = 1006;
+		Exposer exposer = seckillService.exportSeckillUrl(id);
+		if (exposer.isExposed()) {
+			logger.info("exposer={}",exposer);
+			long phone = 18668251985L;
+			String md5 = exposer.getMd5();
+			try {
+				SeckillExecution execution = seckillService.executeSeckill(id, phone, md5);
+				logger.info("excution={}",execution);
+			} catch (RepeatKillException e) {
+				logger.error(e.getMessage());
+			}catch (SeckillCloseException e) {
+				logger.error(e.getMessage());
+			}
+		}else {
+			// 秒杀为开启
+			logger.warn("exposer={}",exposer);
+		}
+	}
+}
