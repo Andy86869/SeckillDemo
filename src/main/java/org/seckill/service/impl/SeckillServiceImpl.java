e3496b3ed8f1ac414a9969ddaa349dfc38620387
Andy <robbincen@163.com>
2016-09-23 20:09:15 +0800

    秒杀优化

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -1,8 +1,11 @@
 package org.seckill.service.impl;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections.MapUtils;
 import org.seckill.dao.SeckillDao;
 import org.seckill.dao.SuccessKilledDao;
 import org.seckill.dto.Exposer;
@@ -20,11 +23,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
+
 @Service
 public class SeckillServiceImpl implements SeckillService {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	//注入到service
+	// 注入到service
 	@Autowired
 	private SeckillDao seckillDao;
 	@Autowired
@@ -42,6 +46,7 @@ public class SeckillServiceImpl implements SeckillService {
 	}
 
 	public Exposer exportSeckillUrl(long seckillId) {
+		// 优化点:缓存优化
 		Seckill seckill = seckillDao.queryById(seckillId);
 		if (seckill == null) {
 			return new Exposer(false, seckillId);
@@ -62,12 +67,10 @@ public class SeckillServiceImpl implements SeckillService {
 		String md5 = DigestUtils.md5DigestAsHex(base.getBytes());
 		return md5;
 	}
-	
+
 	@Transactional
 	/**
-	 * 使用注解控制事务方法的优点:
-	 * 1.开发团队达成一致的约定,明确标注事务方法的编程风格
-	 * 2.使得事务方法的执行时间尽可能短.
+	 * 使用注解控制事务方法的优点: 1.开发团队达成一致的约定,明确标注事务方法的编程风格 2.使得事务方法的执行时间尽可能短.
 	 * 3.不是所有的方法都需要事务,eg:只有一条修改操作,只读操作不需要事务控制.
 	 */
 	public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5)
@@ -79,19 +82,20 @@ public class SeckillServiceImpl implements SeckillService {
 		// 执行秒杀逻辑 : 减库存 + 记录秒杀行为
 		Date nowTime = new Date();
 		try {
-			int updateCount = seckillDao.reduceNumber(seckillId, nowTime);
-			if (updateCount <= 0) {
-				// 没有更新记录,秒杀结束
-				throw new SeckillCloseException("Seckill is closed");
+			// 记录购买行为
+			int insertCount = successKilledDao.insertSuccessKilled(seckillId, userPhone);
+			// 唯一的:根据 seckillId, userPhone 限定不能重复秒杀
+			if (insertCount <= 0) {
+				// 重复秒杀
+				throw new RepeatKillException("Seckill Repeat!");
 			} else {
-				// 记录购买行为
-				int insertCount = successKilledDao.insertSuccessKilled(seckillId, userPhone);
-				// 唯一的:根据 seckillId, userPhone 限定不能重复秒杀
-				if (insertCount <= 0) {
-					// 重复秒杀
-					throw new RepeatKillException("Seckill Repeat!");
+				// 减库存,热点商品的竞争
+				int updateCount = seckillDao.reduceNumber(seckillId, nowTime);
+				if (updateCount <= 0) {
+					// 没有更新记录,秒杀结束 rollback
+					throw new SeckillCloseException("Seckill is closed");
 				} else {
-					// 秒杀成功
+					// 秒杀成功 commit
 					SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillId, userPhone);
 					return new SeckillExecution(seckillId, SeckillStatEnum.SUCCESS, successKilled);
 				}
@@ -105,4 +109,33 @@ public class SeckillServiceImpl implements SeckillService {
 			throw new SeckillException("seckill inner error:" + e.getMessage());
 		}
 	}
+
+	public SeckillExecution executeSeckillProcedure(long seckillId, long userPhone, String md5) {
+		if (md5 == null || !md5.equals(getMD5(seckillId))) {
+			return new SeckillExecution(seckillId, SeckillStatEnum.DATA_REWRITE);
+		}
+		Date killTime = new Date();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("seckillId", seckillId);
+		map.put("phone", userPhone);
+		map.put("kilTime", killTime);
+		map.put("result", null);
+		// 执行存储过程,result 被赋值
+		try {
+			seckillDao.killByProcedure(map);
+			// 获取result
+			int result = MapUtils.getInteger(map, "result", -2);
+			if (result == 1) {
+				SuccessKilled sk = successKilledDao.queryByIdWithSeckill(seckillId, userPhone);
+				return new SeckillExecution(seckillId, SeckillStatEnum.SUCCESS, sk);
+			} else {
+				return new SeckillExecution(seckillId, SeckillStatEnum.stateOf(result));
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return new SeckillExecution(seckillId, SeckillStatEnum.INNER_ERROR);
+		}
+
+	}
+
 }
